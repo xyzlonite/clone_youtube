d6e860c92bbc40e05d7e33b42201a3eda60e124e
Ji Min Bang <interrobang@Jiui-MacBookPro.local>
2022-12-02 06:58:17 +0900

    03. Add Express and Morgan

---
+++ src/server.js
@@ -0,0 +1,22 @@
+import express from 'express';
+import morgan from 'morgan';
+
+const PORT = 4000;
+
+const app = express();
+const logger = morgan('dev');
+
+const home = (req, res) => {
+  return res.send('home');
+};
+const login = (req, res) => {
+  return res.send('login');
+};
+
+app.use(logger);
+app.get('/', home);
+app.get('/login', login);
+
+const handleListening = () => console.log(`Server listenting on port http://localhost:${PORT}`);
+
+app.listen(PORT, handleListening);
