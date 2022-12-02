32ac53872ca3ff82c6d593bcc2785258752ff8b8
Ji Min Bang <interrobang@Jiui-MacBookPro.local>
2022-12-02 16:54:07 +0900

    04. Add Routers

---
+++ src/controllers/videoController.js
@@ -0,0 +1,6 @@
+export const trending = (req, res) => res.send('trending');
+export const see = (req, res) => res.send('Watch Video');
+export const edit = (req, res) => res.send('Edit Video');
+export const remove = (req, res) => res.send('Delete Video');
+export const search = (req, res) => res.send('Search Video');
+export const upload = (req, res) => res.send('Upload Video');
--- src/server.js
+++ src/server.js
@@ -1,21 +1,18 @@
 import express from 'express';
 import morgan from 'morgan';
+import globalRouter from './routers/globalRouter.js';
+import userRouter from './routers/userRouter.js';
+import videoRouter from './routers/videoRouter.js';
 
 const PORT = 4000;
 
 const app = express();
 const logger = morgan('dev');
-
-const home = (req, res) => {
-  return res.send('home');
-};
-const login = (req, res) => {
-  return res.send('login');
-};
-
 app.use(logger);
-app.get('/', home);
-app.get('/login', login);
+
+app.use('/', globalRouter);
+app.use('/users', userRouter);
+app.use('/videos', videoRouter);
 
 const handleListening = () => console.log(`Server listenting on port http://localhost:${PORT}`);
 
