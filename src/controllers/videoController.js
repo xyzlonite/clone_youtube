97cfbbd933737203790e6655a1b50ad6576401dd
xyzlonite <xyzlonite@gmail.com>
2022-12-03 11:46:05 +0900

    05. Add Templates

--- src/controllers/videoController.js
+++ src/controllers/videoController.js
@@ -1,4 +1,36 @@
-export const trending = (req, res) => res.send('trending');
+export const trending = (req, res) => {
+  const videos = [
+    {
+      title: 'Hello',
+      title: 'First Video',
+      rating: 5,
+      comments: 2,
+      createdAt: '2 minutes ago',
+      views: 59,
+      id: 1,
+    },
+    {
+      title: 'Video #2',
+      title: 'Second Video',
+      rating: 5,
+      comments: 2,
+      createdAt: '2 minutes ago',
+      views: 59,
+      id: 1,
+    },
+    {
+      title: 'Whatsup',
+      title: 'Third Video',
+      rating: 5,
+      comments: 2,
+      createdAt: '2 minutes ago',
+      views: 59,
+      id: 1,
+    },
+  ];
+
+  res.render('home', { pageName: 'Home', videos });
+};
 export const see = (req, res) => res.send('Watch Video');
 export const edit = (req, res) => res.send('Edit Video');
 export const remove = (req, res) => res.send('Delete Video');
--- src/server.js
+++ src/server.js
@@ -10,6 +10,9 @@ const app = express();
 const logger = morgan('dev');
 app.use(logger);
 
+app.set('view engine', 'pug');
+app.set('views', process.cwd() + '/src/views');
+
 app.use('/', globalRouter);
 app.use('/users', userRouter);
 app.use('/videos', videoRouter);
